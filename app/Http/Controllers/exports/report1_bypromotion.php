b1d33480f6f71c426e3dfa28b9c17dac867073d4
hchackzcrackzltd <hchackzcrackzltd@gmail.com>
2018-06-04 01:47:29 +0700

    Report Export To Excel Init

---
+++ app/Http/Controllers/exports/report1_bypromotion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\exports;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class report1_bypromotion extends Controller
+{
+    //
+}
---
+++ app/Model/Masterdata/report1_promotionbyexpense.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Masterdata;
+
+use Illuminate\Database\Eloquent\Model;
+
+class report1_promotionbyexpense extends Model
+{
+    protected $table='report1_promotionbyexpense';
+    public $timestamps=false;
+}
